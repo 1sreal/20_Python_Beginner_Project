415c46c4028d1a83073e7dcde3bb77847eaf6809
1sreal <niibenedict123@gmail.com>
2023-01-07 23:06:43 +0000

    raedme

--- emailSender/emailSender.py
+++ emailSender/emailSender.py
@@ -24,3 +24,11 @@ context = ssl.create_default_context()
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as smtp:
     smtp.login(email_sender, email_password)
     smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+'''def replace_word():
+
+    str = "I was a great person in future"
+    word_to_replace = input("Enter the word you want to replace: ")
+    word_replacement = input("Enter the new word: ")
+    print(str.replace(word_to_replace, word_replacement))
+replace_word()
