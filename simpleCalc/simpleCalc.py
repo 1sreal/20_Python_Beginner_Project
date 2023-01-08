6b3a9441202a110b1e6b496e8d42e77f1841c3d1
1sreal <niibenedict123@gmail.com>
2023-01-08 22:15:56 +0000

    simpleCalc

--- simpleCalc/simpleCalc.py
+++ simpleCalc/simpleCalc.py
@@ -1,40 +1,40 @@
 def add(a, b):
     result = a + b
-    print(str(a)+"+"+str(b)+"="+str(result))
+    print(str(a)+" + "+str(b)+" = "+str(result))
 
 def sub(a,b):
     result = a - b
-    print(str(a)+"+"+str(b)+"="+str(result))
+    print(str(a)+" - "+str(b)+" = "+str(result))
 
 def mul(a,b):
     result = a * b
-    print(str(a)+"+"+str(b)+"="+str(result))
+    print(str(a)+" * "+str(b)+" = "+str(result))
 
 def div(a, b):
     result = a / b
-    print(str(a)+"+"+str(b)+"="+str(result))
+    print(str(a)+" / "+str(b)+" = "+str(result))
 
 print(" A. Addition\n B. Subtaction\n C. Multiplication\n D. Division\n E. Exit\n")
 choice = input("Which operation are we performing today?\n")
-if (choice.lower()) == a:
+if (choice.lower() ) == "a":
     print("Addition")
     a = int(input("First number: "))
     b = int(input("Second numer: "))
     add(a,b)
-elif (choice.lower()) == b:
+elif (choice.lower()) == "b":
     print("Subtraction")
     a = int(input("First number: "))
     b = int(input("Second numer: "))
     sub(a,b)
-elif (choice.lower()) == c:
+elif (choice.lower()) == "c":
     print("Multiplication")
     a = int(input("First number: "))
     b = int(input("Second numer: "))
     mul(a,b)
-elif (choice.lower()) == d:
+elif (choice.lower()) == "d":
     print("Division")
     a = int(input("First number: "))
     b = int(input("Second numer: "))
     div(a,b)
-elif (choice.lower()) == e:
+elif (choice.lower()) == "e":
     print("Thanks for your time.")
