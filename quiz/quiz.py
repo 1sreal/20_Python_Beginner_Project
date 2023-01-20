5b6dbaeec562aed91a397a13728f80addf01a903
1sreal <niibenedict123@gmail.com>
2023-01-20 20:34:31 +0000

    quiz_game

---
+++ quiz/quiz.py
@@ -0,0 +1,20 @@
+#create a dictionary that stores the questions and answer in a key-value pair
+#variables that stores answers 
+# taking the answers and grading them for scores
+# tell them if they're right or wrong
+#finally printing the results
+qiuz = {
+    "question1": {
+        "question": "How many regions are in ghana"
+        "answer" : ["16", "sixteen"]
+    }
+    "question2": {
+      "question": "What is the capital town of western north region "
+        "answer" : "sefwi wiaso "  
+    }
+    "question3": {
+        "question": "How many years did the first president of the Republic of Ghana rule?"
+        "answer" : "11 "
+    }
+    
+}
